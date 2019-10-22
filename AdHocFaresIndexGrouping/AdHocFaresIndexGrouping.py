71c2e9bb702bca8c610694fc7770c190ce56fe1b
Greg Williams <greggwilliams58@hotmail.com>
2019-10-22 15:47:35 +0100

    Revert "Ad hoc test"
    This reverts commit 58aaee14016cc3466a514f29f2bd4b977763ae5f.

--- AdHocFaresIndexGrouping/AdHocFaresIndexGrouping.py
+++ AdHocFaresIndexGrouping/AdHocFaresIndexGrouping.py
@@ -3,8 +3,7 @@ import pandas as pd
 
 
 def main():
-    #Lucy wouldn't make a comment.  No comment here
-
+     # Testing to commit
     #additional test
     #test change to demonstrate github functionality
     #variables holding the location and name of the superfile, as well as the location where the result is sent to 
